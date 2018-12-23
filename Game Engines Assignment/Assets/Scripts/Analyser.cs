bbe0b70cfbaa530bd1b87e02e2f4b3a56f5d9cb2
Carl <alterego01010@gmail.com>
2018-12-23 17:52:50 +0000

    Added Analyser script

---
+++ Game Engines Assignment/Assets/Scripts/Analyser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Analyser : MonoBehaviour {
+
+    AudioSource audioSource;
+    float[] spectrum;
+    const int NUM_BINS = 1024;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        spectrum = new float[NUM_BINS];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+	}
+}
